770d0dc4422ec6dccf2ecb58a4093bbbe60b3058
Benjamin Kindle <20398475+literalpie@users.noreply.github.com>
2026-05-09 15:33:45 -0400

    stuff

--- src/lib/getPublicUrl.ts
+++ src/lib/getPublicUrl.ts
@@ -1,9 +1,12 @@
 export const getPublicUrl = () => {
   const serverEnv = typeof process !== 'undefined' ? process.env : undefined
-  const envUrl = import.meta.env.PROD
+  const isProduction =
+    serverEnv?.CONTEXT === 'production' || import.meta.env.VITE_IS_PROD === 'true'
+  const envUrl = isProduction
     ? import.meta.env.VITE_URL
     : import.meta.env.VITE_PUBLIC_URL
   const url =
+    serverEnv?.PUBLIC_URL ||
     envUrl ||
     serverEnv?.URL ||
     serverEnv?.DEPLOY_PRIME_URL ||
